55636e4427fb9375d3f8475ce6b7e82c01f15fd3
moses <moses.kamau@student.moringaschool.com>
2023-06-22 11:17:49 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() { 
+    const info = document.querySelector('p');
+    
+    document.getElementById('text').textContent = "This is really cool!";
+
+});
+console.log("DOM not yet loaded");
